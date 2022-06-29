3a1a41118bb4a3e80349b72245ff2da1a840fbea
ipol88 <94164952+ipol88@users.noreply.github.com>
2022-06-30 02:04:36 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// untuk menambahkan tinggal copy yang ada terus paste di bawahnya, 
+// ganti kode negara sesuai countrycode.org
+
+$linkOffer = array( 
+    "ALL" => "https://www.detik.com", // Yang ini jangan dihapus, sebagai default
+    "US" => "https://smrturl.co/o/158541/1194320?s1=saben",
+    "SG" => "https://smrturl.co/o/158541/1194320?s1=saben",
+);
